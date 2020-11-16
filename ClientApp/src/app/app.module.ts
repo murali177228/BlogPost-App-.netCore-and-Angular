d6c6b71c4944551e68f42b94f2aefcf25f04cdda
murali177228 <murali177228@gmail.com>
2020-11-16 23:44:40 +0200

    New components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogPostsComponent } from './blog-posts/blog-posts.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
+import { BlogPostAddEditComponent } from './blog-post-add-edit/blog-post-add-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostsComponent,
+    BlogPostComponent,
+    BlogPostAddEditComponent
   ],
   imports: [
     BrowserModule,
