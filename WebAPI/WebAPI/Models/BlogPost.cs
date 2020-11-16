d43511e306668390a1e588a3d7f09d290f45b221
murali177228 <murali177228@gmail.com>
2020-11-16 13:10:04 +0200

    Initial update

---
+++ WebAPI/WebAPI/Models/BlogPost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class BlogPost
+    {
+		[Key]
+		public int PostId { get; set; }
+
+		[Required]
+		public string Creator { get; set; }
+
+		[Required]
+		public string Title { get; set; }
+
+		[Required]
+		public string Body { get; set; }
+
+		[Required]
+		public DateTime Dt { get; set; }
+	}
+}
